f564e33fab06302187d333c31969d004ca347a21
totto2727 <kaihatu.totto2727@gmail.com>
2024-05-31 00:40:17 +0900

    feat: apply style

--- pages/api/tina/[...routes].ts
+++ pages/api/tina/[...routes].ts
@@ -1,24 +1,25 @@
-import { TinaNodeBackend, LocalBackendAuthProvider } from "@tinacms/datalayer";
+import { LocalBackendAuthProvider, TinaNodeBackend } from "@tinacms/datalayer";
 
-import { TinaAuthJSOptions, AuthJsBackendAuthProvider } from "tinacms-authjs";
+import { AuthJsBackendAuthProvider, TinaAuthJSOptions } from "tinacms-authjs";
 
 import databaseClient from "../../../tina/__generated__/databaseClient";
 
 const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === "true";
 
 const handler = TinaNodeBackend({
-  authProvider: isLocal
-    ? LocalBackendAuthProvider()
-    : AuthJsBackendAuthProvider({
-        authOptions: TinaAuthJSOptions({
-          databaseClient: databaseClient,
-          secret: process.env.NEXTAUTH_SECRET!,
-        }),
-      }),
-  databaseClient,
+	authProvider: isLocal
+		? LocalBackendAuthProvider()
+		: AuthJsBackendAuthProvider({
+				authOptions: TinaAuthJSOptions({
+					databaseClient: databaseClient,
+					// biome-ignore lint/style/noNonNullAssertion: <explanation>
+					secret: process.env.NEXTAUTH_SECRET!,
+				}),
+			}),
+	databaseClient,
 });
 
 export default (req, res) => {
-  // Modify the request here if you need to
-  return handler(req, res);
+	// Modify the request here if you need to
+	return handler(req, res);
 };
--- tina/config.tsx
+++ tina/config.tsx
@@ -1,30 +1,30 @@
+import { LocalAuthProvider, defineConfig } from "tinacms";
 import {
-  UsernamePasswordAuthJSProvider,
-  TinaUserCollection,
+	TinaUserCollection,
+	UsernamePasswordAuthJSProvider,
 } from "tinacms-authjs/dist/tinacms";
-import { defineConfig, LocalAuthProvider } from "tinacms";
 
 import { PageCollection } from "./collections/page";
 
 const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === "true";
 
 export default defineConfig({
-  authProvider: isLocal
-    ? new LocalAuthProvider()
-    : new UsernamePasswordAuthJSProvider(),
-  contentApiUrlOverride: "/api/tina/gql",
-  build: {
-    publicFolder: "public",
-    outputFolder: "admin",
-  },
-  media: {
-    tina: {
-      mediaRoot: "",
-      publicFolder: "public",
-      static: true,
-    },
-  },
-  schema: {
-    collections: [TinaUserCollection, PageCollection],
-  },
+	authProvider: isLocal
+		? new LocalAuthProvider()
+		: new UsernamePasswordAuthJSProvider(),
+	contentApiUrlOverride: "/api/tina/gql",
+	build: {
+		publicFolder: "public",
+		outputFolder: "admin",
+	},
+	media: {
+		tina: {
+			mediaRoot: "",
+			publicFolder: "public",
+			static: true,
+		},
+	},
+	schema: {
+		collections: [TinaUserCollection, PageCollection],
+	},
 });
